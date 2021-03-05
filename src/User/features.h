438172ff5f5557597c3b4b9646ed4c650254b8d9
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2021-03-05 11:22:12 +0100

    Enable LCD backlight beeing controlled by PWM

--- src/User/features.h
+++ src/User/features.h
@@ -7,7 +7,7 @@
 #define SPI_RESTART_KNOB_PRESS_DURATION 3
 
 // Enable LCD backlight beeing controlled by PWM
-//#define LCD_PWM_DIMMER
+#define LCD_PWM_DIMMER
 #if defined(LCD_PWM_DIMMER)
     #define LCD_LED_PWM_ON_BRIGHTNESS 100
     #define LCD_LED_PWM_OFF_BRIGHTNESS 2
